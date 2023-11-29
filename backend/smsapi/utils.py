88e7e948534998e90d433b99f4587221e111f3c7
mestryghost <argwingsmugendi@gmail.com>
2023-11-29 23:33:31 +0300

    Utils

--- backend/smsapi/utils.py
+++ backend/smsapi/utils.py
@@ -8,15 +8,9 @@ def generateOTP():
     otp = ""
 
     # Write logic for a simple otp generation using pyotp module
-    totp = pyotp.TOTP('base32secret3232')
-    totp.now() # => '492039'
-        
 
-    # OTP verified for current time
-    totp.verify('492039') # => True
-    time.sleep(30)
-    totp.verify('492039') # => False
-    pyotp.random_hex()
+    totp = pyotp.TOTP('base32secret3232')
+    otp = totp.now()
 
     return otp
 
