feff641ed54cef79783df83ab9c8ffb33c8b8578
mestryghost <argwingsmugendi@gmail.com>
2023-11-29 17:15:40 +0300

    Managers and Env

--- backend/sms/settings.py
+++ backend/sms/settings.py
@@ -10,17 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Env Config Setting
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Env Search Path
+environ.Env.read_env( BASE_DIR / '.env' )
+
+SECRET_KEY = env('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tmw^+^e&cxttin-65gz+9p7iymxp6_&j4kgmidfrijpt7rlc@$'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
