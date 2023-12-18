2fb9188cccda08c8942ca243db14820e96768bb6
mestryghost <argwingsmugendi@gmail.com>
2023-12-18 19:27:46 +0300

    Serializers

--- backend/smsapi/serializers.py
+++ backend/smsapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from smsapi.models import User, Teacher, Student
+from smsapi.models import User, Teacher, Student, Subject, studentPerformance, studentScore, Term, Test
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
@@ -15,3 +15,27 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = Student
         fields = ['id', 'username', 'password', 'role', 'first_name', 'last_name', 'email']
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        models = Subject
+        fields = '__all__'
+
+class TermSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        models = Term
+        fields = '__all__'
+
+    def create(self, validated_data):
+        term = super().create(validated_data)
+        self.generate_tests_for_term(term)
+        return term
+    
+    def generate_tests_for_term(self, term):
+        # Assuming a list of subjects is in place
+        subjects = Subject.objects.all()
+
+        for subject in subjects:
+            for test_number in range(1, 3):
+                test_name = f"{subject.name} {chr(ord('A') + test_number - 1)}"
+                Test.objects.create(term=term, subject=subject, name=test_name)
