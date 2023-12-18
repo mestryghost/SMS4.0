47df19dfa7b92cc4d9b53990b4e35061ab9f84cd
mestryghost <argwingsmugendi@gmail.com>
2023-12-18 20:04:20 +0300

    Admin

--- backend/smsapi/admin.py
+++ backend/smsapi/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from smsapi.models import User, Teacher, Student
+from smsapi.models import User, Teacher, Student, Test, Term, Subject, studentPerformance, studentScore
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Teacher)
 admin.site.register(Student)
+admin.site.register(Test)
+admin.site.register(Term)
+admin.site.register(Subject)
+admin.site.register(studentPerformance)
+admin.site.register(studentScore)
